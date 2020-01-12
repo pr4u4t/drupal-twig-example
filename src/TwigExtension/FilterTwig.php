7f8a5bf8af84720999f22fb5a2f3f170d6ea6d05
Paweł Ciejka <pawel.ciejka@robco.pl>
2020-01-12 01:24:40 +0000

    syntax fixes

--- src/TwigExtension/FilterTwig.php
+++ src/TwigExtension/FilterTwig.php
@@ -1,30 +1,30 @@
-namespace Drupal\robco_twig\TwigExtension; 
+<?php
+	namespace Drupal\robco_twig\TwigExtension; 
 
-use Drupal\robco_twig\TwigExtension\HtmlFilter;
+	use Drupal\robco_twig\TwigExtension\HtmlFilter;
  
-class FilterTwig extends \Twig_Extension {    
+	class FilterTwig extends \Twig_Extension {    
  
-  /**
-   * Generates a list of all Twig filters that this extension defines.
-  */
-  public function getFilters() {
-    return [
-      new \Twig_SimpleFilter('htmlcontent', array($this, 'htmlContent')),
-    ];
-  }
+  	/**
+   	* Generates a list of all Twig filters that this extension defines.
+  	*/
+  	public function getFilters() {
+    		return [
+      			new \Twig_SimpleFilter('htmlcontent', array($this, 'htmlContent')),
+    		];
+  	}
  
-  /**
-   * Gets a unique identifier for this Twig extension.
-   */
-  public function getName() {
-    return 'robco_twig.twig_extension';
-  }
+  	/**
+   	* Gets a unique identifier for this Twig extension.
+   	*/
+  	public function getName() {
+    		return 'robco_twig.twig_extension';
+  	}
  
-  /**
-   * Replaces all numbers from the string.
-   */
-  public static function htmlContent($html,$attr,$value) {
-  	return HtmlFilter::getContent($html,$attr,$value); 
-  }
- 
+  	/**
+   	* Replaces all numbers from the string.
+   	*/
+  	public static function htmlContent($html,$attr,$value) {
+  		return HtmlFilter::getContent($html,$attr,$value); 
+  	}
 }
