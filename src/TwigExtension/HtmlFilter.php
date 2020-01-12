d800f65ea74b200b6fa9553ec65684bf7bd951ee
Pawel Ciejka <pawel.ciejka@robco.pl>
2020-01-12 01:48:37 +0000

    HtmlFilter namespace fixes

--- src/TwigExtension/HtmlFilter.php
+++ src/TwigExtension/HtmlFilter.php
@@ -4,12 +4,12 @@
 	class HtmlFilter{
 
 		public static function getContent($html,$attr,$value){
-			if(!($dom = new DOMDocument()))	return -1;
+			if(!($dom = new \DOMDocument())) return -1;
 			$dom->loadHTML($html);
-			if(!($xpath = new DOMXpath($dom))) return -2;
+			if(!($xpath = new \DOMXpath($dom))) return -2;
 			if(!($result = $xpath->query("//*[@$attr = '$value']"))) return -3;
 			if(!($keepme = $result->item(0))) return -4;
-			if(!($tempDom = new DOMDocument())) return -5;
+			if(!($tempDom = new \DOMDocument())) return -5;
 			if(!($tempImported = $tempDom->importNode($keepme, true))) return -6;
 			$tempDom->appendChild($tempImported);
 			if(!($newHtml = $tempDom->saveHTML())) return -7;
